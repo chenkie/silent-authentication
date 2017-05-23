565949dc016ca87e75d98c59a70425ed079f7f79
Ryan Chenkie <ryanchenkie@gmail.com>
2017-05-23 18:47:44 -0400

    fix callback

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -8,6 +8,6 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'w4ibtscMzP2Zs3jk6MteHwXZ422gGyQc',
   domain: 'blogtest.auth0.com',
-  callbackURL: 'https://nameless-sea-38333.herokuapp.com',
+  callbackURL: 'https://nameless-sea-38333.herokuapp.com/callback',
   apiUrl: 'https://api.test.com'
 };
